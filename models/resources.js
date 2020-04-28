8fa2a27d3f12f6a06d97264459fc3407ddf342ba
Michael Cassaro <mdcassaro@hotmail.com>
2020-04-28 11:50:13 -0500

    linted the resource files

--- models/resources.js
+++ models/resources.js
@@ -1,35 +1,34 @@
-module.exports = function(sequelize, Datatypes){
-    var Resources = sequelize.define("Resources", {
-        fields: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        title:{
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        description: {
-            type: Datatypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        }
-
+module.exports = function(sequelize, Datatypes) {
+  var Resources = sequelize.define("Resources", {
+    fields: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    title: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    description: {
+      type: Datatypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+  Resources.associate = function(models) {
+    Resources.belongsTo(models.Fields, {
+      foreignKey: {
+        allowNull: false
+      }
     });
-    Resources.associate = function(models){
-        Resources.belongsTo(models.Fields,{
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
+  };
 
-    return Resources;
-}
+  return Resources;
+};
