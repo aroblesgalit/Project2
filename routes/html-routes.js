903e7ef626a55e0f8adb67c32c6670c6a67bc491
Alvin Galit <aroblesgalit@gmail.com>
2020-04-28 22:14:57 -0500

    Added html route for signup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -29,6 +29,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
+  app.get("/signup", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/userProfile", isAuthenticated, function(req, res) {
