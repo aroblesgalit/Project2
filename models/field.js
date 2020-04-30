dc2ab6b7333fec38b0da89bf05074cc20d412800
Lee Ann Norman <leeann.norm@gmail.com>
2020-04-30 11:16:03 -0500

    able to correct issues with Travis -- ran npm run test -- used npm run fix. Able to save file now

--- models/field.js
+++ models/field.js
@@ -1,23 +1,22 @@
 // Creating our Titles model
-export default function (sequelize, Datatypes) {
-    let Fields = sequelize.define("Fields", {
-        titles: {
-            type: Datatypes.STRING,
-            validate: {
-                len: [1]
-            }
-        }
+module.exports = function(sequelize, Datatypes) {
+  let Fields = sequelize.define("Fields", {
+    titles: {
+      type: Datatypes.STRING,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  // Associate table with Resources table
+  Fields.associate = function(models) {
+    Fields.hasMany(models.Resources, {
+      foreignKey: {
+        allowNull: false
+      }
     });
+  };
 
-// Associate table with Resources table
-    Fields.associate = function (models) {
-        Fields.hasMany(models.Resources, {
-            foreignKey: {
-                allowNull: false
-            }
-        })
-    };
-
-    return Fields;
+  return Fields;
 };
-
