83ad09d1528251cc79b6d34f782ec818a16f8e34
Lee Ann Norman <leeann.norm@gmail.com>
2020-05-01 12:28:00 -0500

    added post request for api field routes

--- routes/api-field-routes.js
+++ routes/api-field-routes.js
@@ -36,6 +36,46 @@ module.exports = function(app) {
   }).then(function(dbResources) {
     res.json(dbResources);
   });
+    //retrieves a Field post
+  app.get("/api/field/:id", function(req, res) {
+    db.Fields.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbFields) {
+      console.log(dbFields);
+      res.json(dbFields);
+    });
+  });
+
+//   creates and saves new fields
+  app.post("/api/field", function(req, res) {
+    db.Fields.create(req.body).then(function(dbFields) {
+      res.json(dbFields);
+    });
+  });
+
+//   deletes fields
+  app.delete("/api/field/:id", function(req, res) {
+    db.Fields.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbFields) {
+      res.json(dbFields);
+    });
+  });
+
+//   updates fields
+  app.put("/api/field", function(req, res) {
+    db.Fields.update(req.body, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbFields) {
+      res.json(dbFields);
+    });
+  });
 };
 
-app.get("/api/fields");
+
