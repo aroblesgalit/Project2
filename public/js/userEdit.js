1ae105fe9b35634ea6a545695881ae89a1d6385d
fruityvegetables <gg.troyer@gmail.com>
2020-04-30 21:32:59 -0500

    fixed link to edit profile page, and the extra column in our user table causing errors

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -1,31 +1,31 @@
 $(document).ready(function() {
-  //sets our updating variable to false initially
-  var updating = false;
-  //sets this variable to plug into our user's field choice html element
-  var userField = $("#userFieldRenderChoice");
-  // handles the form submission when the user enters a field and hits submit
-  $(userFieldForm).on("submit", handleFormSubmit);
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    // Wont submit the change if we are missing a string
-    if (!userField.val().trim()) {
-      return;
-    }
-    // Constructing a newPost object to hand to the database
-    // var newFieldChoice = {
-    //   title: fieldTitle.val().trim(),
-    //   fieldId: authorSelect.val()
-    // };
+  // //sets our updating variable to false initially
+  // var updating = false;
+  // //sets this variable to plug into our user's field choice html element
+  // var userField = $("#userFieldRenderChoice");
+  // // handles the form submission when the user enters a field and hits submit
+  // $(userFieldForm).on("submit", handleFormSubmit);
+  // function handleFormSubmit(event) {
+  //   event.preventDefault();
+  //   // Wont submit the change if we are missing a string
+  //   if (!userField.val().trim()) {
+  //     return;
+  //   }
+  //   // Constructing a newPost object to hand to the database
+  //   // var newFieldChoice = {
+  //   //   title: fieldTitle.val().trim(),
+  //   //   fieldId: authorSelect.val()
+  //   // };
 
-    // If we're updating a post run updatePost to update a post
-    // Otherwise run submitPost to create a whole new post
-    if (updating) {
-      newPost.id = postId;
-      updatePost(newPost);
-    } else {
-      submitPost(newPost);
-    }
-  }
+  //   // If we're updating a post run updatePost to update a post
+  //   // Otherwise run submitPost to create a whole new post
+  //   if (updating) {
+  //     newPost.id = postId;
+  //     updatePost(newPost);
+  //   } else {
+  //     submitPost(newPost);
+  //   }
+  // }
 
   $("#profileImage").click(function() {
     $("#imageUpload").click();
