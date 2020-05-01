b80aafafbaf6d46e861b8a23e60818d7cc9fd54e
Alvin Galit <aroblesgalit@gmail.com>
2020-04-30 22:55:31 -0500

    Added comments to the resources js file

--- public/js/resources.js
+++ public/js/resources.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-  // resourceResults holds all of our resources
+  // Target div that will contain the results for resources
   var resourceResults = $(".resource-results");
-  // Variable to hold our resources
+  // Declare a variable to hold our resources
   var resources;
-  // Getting references to our form and inputs
+  // Get reference to our form and inputs
   var fieldsSelect = $("#fieldsSelect");
 
   // Render fields select options
   getFields();
 
+  // Add an change event to the fieldsSelect dropdown
   fieldsSelect.change(function() {
-    // Grab the id of the selected field
+    // Grab the id of the selected field option
     var selectedFieldId = $(this)
       .children("option:selected")
       .val();
+    // Get the resources for the selected field using the id
     getResources(selectedFieldId);
   });
 
+  // Declare a function to render the fields into the select dropdown
   function getFields() {
     $.get("/api/fields")
       .then(function(data) {
@@ -34,31 +37,19 @@ $(document).ready(function() {
       });
   }
 
-  // The code below handles the case where we want to get resources for a specific field
-  // Looks for a query param in the url for field_id
-  //   var url = window.location.search;
-  //   var fieldId;
-  //   if (url.indexOf("?field_id=") !== -1) {
-  //     fieldId = url.split("=")[1];
-  //     getResources(fieldId);
-  //   }
-  //   // If there's no fieldId we just get the first one
-  //   else {
-  //     getResources(0);
-  //   }
-
-  // This function grabs resources from the database and updates the view
+  // This function grabs resources from the database and updates the view based on a field id
   function getResources(fieldId) {
-    // fieldId = field || "";
-    // if (fieldId) {
-    //   fieldId = "/?field_id=" + fieldId;
-    // }
+    // Make a get request to /api/resources/ plust the field id
     $.get("/api/resources/" + fieldId)
+      // Then use the data from the request
       .then(function(data) {
         console.log("Resources", data);
+        // Store the data into our variable resources
         resources = data;
+        // If no data then call the displayEmpty function
         if (!resources || !resources.length) {
           displayEmpty();
+          // Else render the results
         } else {
           initializeRows();
         }
@@ -72,11 +63,16 @@ $(document).ready(function() {
 
   // InitializeRows handles appending all of our constructed resource HTML inside resourceResults
   function initializeRows() {
+    // First empty the container
     resourceResults.empty();
+    // Declare a variable for an empty array to hold the resources
     var resourcesToAdd = [];
+    // Loop through the data
     for (var i = 0; i < resources.length; i++) {
+      // Render each data onto a new card and push each into the empty array
       resourcesToAdd.push(createNewCard(resources[i]));
     }
+    // Append it to the container
     resourceResults.append(resourcesToAdd);
   }
 
@@ -85,7 +81,7 @@ $(document).ready(function() {
     // Get the date and format it
     // var formattedDate = new Date(resource.createdAt);
     // formattedDate = moment(formattedDate).format("MMMM Do YYYY, h:mm:ss a");
-    // Create divs
+    // Create the divs
     var newResourceCard = $("<div>");
     var resourceCardWrapper = $("<div>").addClass("uk-card uk-card-default");
     var cardImageDiv = $("<div>").addClass("uk-card-media-top");
@@ -118,6 +114,7 @@ $(document).ready(function() {
     return newResourceCard;
   }
 
+  // This will display a message if there's no data
   function displayEmpty() {
     resourceResults.html(
       "<p>No resources posted for this field yet. Sign up or Log in to add.</p>"
