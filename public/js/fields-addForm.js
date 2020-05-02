4ca7341de78bc5bf6a3ebc0d48723db3201216e3
Alvin Galit <aroblesgalit@gmail.com>
2020-05-02 18:49:11 -0500

    Added comments to the fields-addForm js file

--- public/js/fields-addForm.js
+++ public/js/fields-addForm.js
@@ -1,62 +1,63 @@
 $(document).ready(function() {
   // Target the form
   var addForm = $("#addField");
-  // Getting references to our form and inputs
+  // Get reference to our form input
   var titleField = $("#form-title");
 
   // Get list of fields
   var fieldsList = [];
+  // Render fields select
   getFields();
+  // Function to render fields select
   function getFields() {
+    // Make a get request to /apifields
     $.get("/api/fields")
       .then(function(data) {
         // Expecting data to be an array of "fields" as objects
+        // Loop through array and push the titles of each field into the fieldsList empty array
         for (var i = 0; i < data.length; i++) {
           fieldsList.push(data[i].title);
         }
-        console.log(fieldsList);
       })
       .catch(function(err) {
         console.log(err);
       });
   }
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // Add a submit event listener to the form
   addForm.on("submit", function(event) {
     event.preventDefault();
-
+    // Get user input for the title
     var titleInput = titleField.val().trim();
-
+    // If the title already exists in our list, then alert user and return
     if (fieldsList.indexOf(titleInput) > -1) {
       alert("Field already exists.");
       return;
     }
 
-    // Add resource
+    // Otherwise add the field
     addField(titleInput);
   });
 
-  // Does a post to the resources/:fieldId route. If successful, we are redirected to the resource page
-  // Otherwise we log any errors
+  // Function to add a field given the title input
   function addField(title) {
+    // Make a post request to /api/fields route
     $.post("/api/fields", {
       title: title
     })
+      // Then redirect user to the fields page if successful
       .then(function() {
         window.location.replace("/fields");
-        // If there's an error, handle it by throwing up a bootstrap alert
       })
-      .catch(handleLoginErr);
-  }
-  //this will alert users signing up with an email already being used
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON.original.sqlMessage);
-    //console.log(err);
-    $("#alert").fadeIn(500);
+      // If there's an error, handle it by logging the error to the console
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 
-  // Cancel button logic
+  // Lisent to a click event on the cancel button
   $("#cancelFieldButton").click(function() {
+    // Redirect user to the fields page
     window.location.replace("/fields");
   });
 });
