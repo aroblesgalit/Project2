6e5c2ef491583c9b4d259c53931e81a686dfdbf7
Alvin Galit <aroblesgalit@gmail.com>
2020-05-02 18:57:12 -0500

    Added comments to the resource-addForm js

--- public/js/resources-addForm.js
+++ public/js/resources-addForm.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // Render field select
+  // Target the form
   var addForm = $("#addResource");
   // Getting references to our form and inputs
   var fieldsSelect = $("#form-field");
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   // Render fields select options
   getFields();
-
+  // Function to render fields select options
   function getFields() {
     $.get("/api/fields")
       .then(function(data) {
@@ -25,19 +25,20 @@ $(document).ready(function() {
         console.log(err);
       });
   }
-
+  // Listen to a change event on the select input and store the value to selectedFieldId
   var selectedFieldId = fieldsSelect.change(function() {
     return $(this)
       .children("option:selected")
       .val();
   });
 
-  // When the signup button is clicked, we validate the email and password are not blank
+  // Listen to a submit event on the form
   addForm.on("submit", function(event) {
     event.preventDefault();
-
+    // Make a get request to /api/user_data
     $.get("/api/user_data")
       .then(function(data) {
+        // Store user inputs into resourceData
         var resourceData = {
           FieldId: parseInt(selectedFieldId.val()),
           title: titleField.val().trim(),
@@ -46,11 +47,11 @@ $(document).ready(function() {
           imageUrl: imageUrlField.val().trim(),
           UserId: data.id
         };
-        console.log(resourceData);
 
         // if (!userData.email || !userData.password) {
         //   return;
         // }
+        // Destructure resourceData
         var {
           FieldId,
           title,
@@ -59,7 +60,7 @@ $(document).ready(function() {
           imageUrl,
           UserId
         } = resourceData;
-        // Add resource
+        // Add resource and pass in the variables from resourceData
         addResource(FieldId, title, description, link, imageUrl, UserId);
         // titleField.val("");
         // descriptionField.val("");
@@ -71,10 +72,11 @@ $(document).ready(function() {
       });
   });
 
-  // Does a post to the resources/:fieldId route. If successful, we are redirected to the resource page
-  // Otherwise we log any errors
+  // Function to add a resource using user inputs
   function addResource(FieldId, title, description, link, imageUrl, UserId) {
+    // Make a post request to /api/resources
     $.post("/api/resources", {
+      // Use user input for appropriate params
       FieldId: FieldId,
       title: title,
       description: description,
@@ -82,21 +84,18 @@ $(document).ready(function() {
       imageUrl: imageUrl,
       UserId: UserId
     })
+      // Redirect user to the resource page
       .then(function() {
         window.location.replace("/resources");
-        // If there's an error, handle it by throwing up a bootstrap alert
       })
-      .catch(handleLoginErr);
-  }
-  //this will alert users signing up with an email already being used
-  function handleLoginErr(err) {
-    $("#alert .msg").text(err.responseJSON.original.sqlMessage);
-    //console.log(err);
-    $("#alert").fadeIn(500);
+      .catch(function(err) {
+        console.log(err);
+      });
   }
 
-  // Cancel button logic
+  // Add a click event to the cancel button
   $("#cancelResourceButton").click(function() {
+    // Redirect user to the resources page
     window.location.replace("/resources");
   });
 });
