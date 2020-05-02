4b52c5b299379b5b06bf96c6d4f5c08afe79c8a3
Alvin Galit <aroblesgalit@gmail.com>
2020-05-01 21:43:59 -0500

    Polished resources page

--- public/js/resources.js
+++ public/js/resources.js
@@ -93,7 +93,9 @@ $(document).ready(function() {
     var cardTitle = $("<h3>")
       .addClass("uk-card-title")
       .text(resource.title);
-    var descriptionDiv = $("<div>").addClass("descript-button uk-flex");
+    var descriptionDiv = $("<div>").addClass(
+      "descript-button uk-flex uk-flex-middle"
+    );
     var description = $("<p>").text(resource.description);
     var linkUrl = $("<a>").attr("href", resource.link);
     var resourceButton = $("<button>").addClass(
